b41427d2e9bf03973c11dfe2225631aa9f29c0f3
Farshidroohi <farshid.roohi.a@gmail.com>
2021-05-16 15:05:54 +0430

    init

---
+++ FarshidFramework/FarshidFramework.h
@@ -0,0 +1,18 @@
+//
+//  FarshidFramework.h
+//  FarshidFramework
+//
+//  Created by Farshid on 5/16/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FarshidFramework.
+FOUNDATION_EXPORT double FarshidFrameworkVersionNumber;
+
+//! Project version string for FarshidFramework.
+FOUNDATION_EXPORT const unsigned char FarshidFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FarshidFramework/PublicHeader.h>
+
+
---
+++ FarshidFramework/Human.swift
@@ -0,0 +1,18 @@
+//
+//  Human.swift
+//  FarshidFramework
+//
+//  Created by Farshid on 5/16/21.
+//
+
+import Foundation
+
+
+public class Human {
+    
+    public static func saySomething() -> String {
+        let value =  "Hello, Human!"
+        print(value)
+        return value
+    }
+}
